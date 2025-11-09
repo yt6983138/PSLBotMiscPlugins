76ed0d025c946e84171dacfedc23fb1017c723cd
yt6983138 <yt6983138@gmail.com>
2025-11-09 11:59:33 +0800

    PhigrosScoreLookup Website ver prototype [undone]

---
+++ PersonalWebsite/wwwroot/personal/js/psl.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../../swagger/PhigrosApi/index.js" />
+/// <reference path="../../swagger/PhigrosApi/ApiClient.js" />
+
+import CloudSaveApi from "../../swagger/PhigrosApi/api/CloudSaveApi";
+import LocalSaveApi from "../../swagger/PhigrosApi/api/LocalSaveApi";
+import LoginQrCodeApi from "../../swagger/PhigrosApi/api/LoginQrCodeApi";
+
+var qrCodeApi = new LoginQrCodeApi();
+var cloudSaveApi = new CloudSaveApi();
+var localSaveApi = new LocalSaveApi();
