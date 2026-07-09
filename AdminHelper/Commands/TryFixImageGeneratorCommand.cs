5cc2933bc4b6feb684ecd8634b8721dd96badd28
static_void <83499886+yt6983138@users.noreply.github.com>
2026-07-09 11:45:52 +0800

    Add command to try fix image generator

---
+++ AdminHelper/Commands/TryFixImageGeneratorCommand.cs
@@ -0,0 +1,34 @@
+﻿using PSLDiscordBot.Core.ImageGenerating;
+
+namespace AdminHelper.Commands;
+
+[AddToGlobal]
+public class TryFixImageGeneratorCommand : AdminCommandBase
+{
+	private readonly ImageGenerator _imageGenerator;
+
+	public TryFixImageGeneratorCommand(IServiceProvider provider, ImageGenerator generator) : base(provider)
+	{
+		this._imageGenerator = generator;
+	}
+
+	public override OneOf<string, LocalizedString> PSLName => "try-fix-image-generator";
+	public override OneOf<string, LocalizedString> PSLDescription => "[Admin command] Try to fix image generator";
+
+	public override SlashCommandBuilder CompleteBuilder => this.BasicBuilder
+		.AddOption(
+			"delay",
+			ApplicationCommandOptionType.Integer,
+			"Delay after dispose and before setting up engine, in seconds",
+			isRequired: false,
+			minValue: 0);
+
+	public override async Task Callback(SocketSlashCommand arg, UserData? data, DataBaseService.DbDataRequester requester, object executer)
+	{
+		int seconds = arg.GetIntegerOptionAsInt32OrDefault("delay", 10);
+
+		await arg.QuickReply("Restarting...");
+		await this._imageGenerator.RestartUnderlyingChromium(TimeSpan.FromSeconds(seconds));
+		await arg.QuickReply("Done");
+	}
+}
